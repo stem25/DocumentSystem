c1b4a8e065a243ae3e5bfb44b5c17a6715bcdbbb
nsychev <stem25@icloud.com>
2018-05-21 15:13:41 +0500

    first commit

---
+++ src/main/java/model/Document.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.util.Date;
+
+public abstract class Document implements Comparable<Document>{
+    private Long id;
+    private String name;
+    private String text;
+    private Long registrationNumber;
+    private Date registrationDate;
+    private String author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(Long registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public int compareTo(Document o) {
+       return 0;
+    }
+}
